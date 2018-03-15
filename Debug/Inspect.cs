b04a84f760c078b5441e85694ac1954539b7e8c8
Leo Terziman <leo.terziman@clarte.asso.fr>
2018-03-15 16:52:17 +0100

    Fixed comments

--- Debug/Inspect.cs
+++ Debug/Inspect.cs
@@ -4,13 +4,13 @@ namespace CLARTE.Debug
 {
 	/// <summary>
 	/// Utility script to easily inspect in debugger the values of internal variables
-	/// of any MonoBehaviour objects in a scene.
+	/// of any object in a scene.
 	/// </summary>
 	/// <remarks>This component can easily be added at runtime to debug specific objects.</remarks>
 	public class Inspect : MonoBehaviour
 	{
 		/// <summary>
-		/// The MonoBehaviour to inspect in the debugger.
+		/// The object to inspect in the debugger.
 		/// </summary>
 		public Object inspected;
 
