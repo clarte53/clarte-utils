f78a474a7188f4509055e40b62f6f3825655e244
Leo Terziman <leo.terziman@clarte.asso.fr>
2018-04-19 18:40:05 +0200

    Added .Net tuple support for unity .Net 2.0 & 3.5

---
+++ DotNet/Collections/IStructuralComparable.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+#if NET_2_0 || NET_2_0_SUBSET
+
+namespace System.Collections
+{
+    public interface IStructuralComparable
+	{
+        Int32 CompareTo(Object other, IComparer comparer);
+    }
+}
+
+#endif
---
+++ DotNet/Collections/IStructuralEquatable.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+#if NET_2_0 || NET_2_0_SUBSET
+
+namespace System.Collections
+{
+    public interface IStructuralEquatable
+	{
+        Boolean Equals(Object other, IEqualityComparer comparer);
+        int GetHashCode(IEqualityComparer comparer);
+    }
+}
+
+#endif
