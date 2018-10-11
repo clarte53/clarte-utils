bfea4e537d5168ebbf80f8a16e28a5897082d0fb
Leo Terziman <leo.terziman@clarte.asso.fr>
2018-10-11 17:23:58 +0200

    Added support for asynchronous procedure call to monobehaviour instances in order to call unity related code inside the main unity thread

---
+++ Threads/APC/MonoBehaviourCall.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+namespace CLARTE.Threads.APC
+{
+	public class MonoBehaviourCall : Pattern.Singleton<MonoBehaviourCall>, ICall
+	{
+		#region Members
+		protected Thread unityThread;
+		protected Queue<Task> pending = new Queue<Task>();
+		protected Queue<Task> inProgress = new Queue<Task>();
+		#endregion
+		
+		#region MonoBehaviour callbacks
+		protected void Awake()
+		{
+			unityThread = Thread.CurrentThread;
+		}
+		
+		protected override void OnDestroy()
+		{
+			unityThread = null;
+			
+			pending.Clear();
+			inProgress.Clear();
+			
+			base.OnDestroy();
+		}
+		
+		protected void Update()
+		{
+			lock(pending)
+			{
+				while(pending.Count > 0)
+				{
+					inProgress.Enqueue(pending.Dequeue());
+				}
+			}
+			
+			while(inProgress.Count > 0)
+			{
+                inProgress.Dequeue().callback();
+			}
+		}
+		#endregion
+		
+		#region ICall implementation
+		public Result<T> Call<T>(Func<T> callback)
+		{
+            Task task = Task.Create(callback);
+
+            Call(task);
+
+            return (Result<T>) task.result;
+        }
+		
+		public Result Call(Action callback)
+		{
+            Task task = Task.Create(callback);
+
+            Call(task);
+
+            return task.result;
+		}
+        #endregion
+
+        #region Internal methods
+        protected void Call(Task task)
+        {
+            if(Thread.CurrentThread != unityThread)
+            {
+                lock(pending)
+                {
+                    pending.Enqueue(task);
+                }
+            }
+            else
+            {
+                task.callback();
+            }
+        }
+        #endregion
+    }
+}
