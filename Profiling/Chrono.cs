7dfe07e01d09c82c6ac701d35fab8a25250b89ed
Lionel <lionel@clarte.asso.fr>
2018-11-29 11:24:52 +0100

    Added code profiling utilities (measurement probes and logging)

---
+++ Profiling/Chrono.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace CLARTE.Profiling
+{
+	/// <summary>
+	/// Tick-accurate chronometer class
+	/// </summary>
+	public class Chrono
+	{
+		private Stopwatch m_stopWatch;
+
+		public Chrono()
+		{
+			m_stopWatch = new Stopwatch();
+		}
+
+		/// <summary>
+		/// Start chronometer
+		/// </summary>
+		public void Start()
+		{
+			m_stopWatch.Start();
+		}
+
+		/// <summary>
+		/// Stop chronometer
+		/// </summary>
+		public void Stop()
+		{
+			m_stopWatch.Stop();
+		}
+
+		/// <summary>
+		/// Reset chronometer
+		/// </summary>
+		public void Reset()
+		{
+			m_stopWatch.Reset();
+		}
+
+		/// <summary>
+		/// Get elapsed time in seconds
+		/// </summary>
+		/// <returns></returns>
+		public double GetElapsedTime()
+		{
+			long elapsed_ticks = m_stopWatch.ElapsedTicks;
+			double elapsed_s = (double)elapsed_ticks / (double)Stopwatch.Frequency;
+
+			return elapsed_s;
+		}
+	}
+}
---
+++ Profiling/LogInFile.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CLARTE.Profiling
+{
+	static public class LogInFile
+	{
+		static private Dictionary<string, List<string>> m_logs;
+
+		/// <summary>
+		/// Add line to the log
+		/// </summary>
+		/// <param name="filename">Log file name</param>
+		/// <param name="log">Line to be added</param>
+		/// <param name="writeImmediately">Should the log be immediately dumped into the file? Otherwise dump will only occur upon manual call to DumpLog() or DumpAllLogs()</param>
+		static public void AddToLog(string filename, System.Object log, bool writeImmediately = false)
+		{
+			if(m_logs == null)
+			{
+				m_logs = new Dictionary<string, List<string>>();
+			}
+
+			if(!m_logs.ContainsKey(filename))
+			{
+				m_logs.Add(filename, new List<string>());
+
+				System.IO.File.Delete(filename);
+			}
+
+			string logStr = log.ToString();
+
+			m_logs[filename].Add(logStr);
+
+			if(writeImmediately)
+			{
+				using(System.IO.StreamWriter file = new System.IO.StreamWriter(filename, true))
+				{
+					file.WriteLine(logStr);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Dump specific log ti file
+		/// </summary>
+		/// <param name="filename"></param>
+		static public void DumpLog(string filename)
+		{
+			if(m_logs != null)
+			{
+				if(m_logs.ContainsKey(filename))
+				{
+					System.IO.File.WriteAllLines(filename, m_logs[filename].ToArray());
+
+					Debug.Log("Log dumped to " + filename);
+				}
+				else
+				{
+					Debug.LogWarning("No log to be dumped into " + filename);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Dump All logs to files
+		/// </summary>
+		static public void DumpAllLogs()
+		{
+			if(m_logs != null)
+			{
+				foreach(string filename in m_logs.Keys)
+				{
+					DumpLog(filename);
+				}
+			}
+		}
+	}
+}
