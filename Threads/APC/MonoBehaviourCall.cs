e9f77755113aa5074fafebf5704993836054f7bf
Leo Terziman <leo.terziman@clarte.asso.fr>
2018-11-30 16:25:28 +0100

    Added some doc

--- Threads/APC/MonoBehaviourCall.cs
+++ Threads/APC/MonoBehaviourCall.cs
@@ -3,6 +3,14 @@ using CLARTE.Pattern;
 
 namespace CLARTE.Threads.APC
 {
+    /// <summary>
+    /// Utility class to execute code in unity thread.
+    /// </summary>
+    /// <remarks>
+    /// This class is based on the Reactor design pattern. It allows various threads
+    /// to call callbacks back in the unity thread and be notified of the end and result
+    /// value of the operation.
+    /// </remarks>
 	public class MonoBehaviourCall : Singleton<MonoBehaviourCall>, ICall
 	{
         #region Members
@@ -24,6 +32,12 @@ namespace CLARTE.Threads.APC
         #endregion
 
         #region Public methods
+        /// <summary>
+        /// Add a task that will return a value.
+        /// </summary>
+        /// <typeparam name="T">The type of the return value.</typeparam>
+        /// <param name="callback">The task to execute.</param>
+        /// <returns>The return value of the task embedded in a Result object.</returns>
         public Result<T> Call<T>(Func<T> callback)
 		{
             Task task = Task.Create(callback);
@@ -33,6 +47,11 @@ namespace CLARTE.Threads.APC
             return (Result<T>) task.result;
         }
 
+        /// <summary>
+        /// A task that will not return a value.
+        /// </summary>
+        /// <param name="callback">The task to execute.</param>
+        /// <returns>An empty Result object to get notified of the task completion and raised exceptions.</returns>
         public Result Call(Action callback)
 		{
             Task task = Task.Create(callback);
