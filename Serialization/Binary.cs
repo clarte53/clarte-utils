5a98fa184ff2e41a463b7aa8a0f1c290c86e6016
Leo Terziman <leo.terziman@clarte.asso.fr>
2018-12-13 11:37:38 +0100

    Added support for hashset in binary serialization

--- Serialization/Binary.cs
+++ Serialization/Binary.cs
@@ -35,6 +35,7 @@ namespace CLARTE.Serialization
 			COLOR,
 			ARRAY,
             LIST,
+            HASHSET,
 			DICTIONARY,
 		}
 
@@ -238,6 +239,7 @@ namespace CLARTE.Serialization
 			{
 				ARRAY,
                 LIST,
+                HASHSET,
 				DICTIONARY
 			}
 
@@ -341,10 +343,12 @@ namespace CLARTE.Serialization
 		private static readonly Dictionary<SupportedTypes, uint> sizes;
 		private static readonly MethodInfo fromBytesArray;
         private static readonly MethodInfo fromBytesList;
-		private static readonly MethodInfo fromBytesDictionary;
+        private static readonly MethodInfo fromBytesHashSet;
+        private static readonly MethodInfo fromBytesDictionary;
 		private static readonly MethodInfo toBytesArray;
         private static readonly MethodInfo toBytesList;
-		private static readonly MethodInfo toBytesDictionary;
+        private static readonly MethodInfo toBytesHashSet;
+        private static readonly MethodInfo toBytesDictionary;
 		private static readonly object[] emptyParameters;
 		private static readonly bool isLittleEndian;
 
@@ -424,7 +428,10 @@ namespace CLARTE.Serialization
                                     case MethodLocatorAttribute.Parameter.LIST:
                                         fromBytesList = method;
                                         break;
-									case MethodLocatorAttribute.Parameter.DICTIONARY:
+                                    case MethodLocatorAttribute.Parameter.HASHSET:
+                                        fromBytesHashSet = method;
+                                        break;
+                                    case MethodLocatorAttribute.Parameter.DICTIONARY:
 										fromBytesDictionary = method;
 										break;
 									default:
@@ -441,7 +448,10 @@ namespace CLARTE.Serialization
                                     case MethodLocatorAttribute.Parameter.LIST:
                                         toBytesList = method;
                                         break;
-									case MethodLocatorAttribute.Parameter.DICTIONARY:
+                                    case MethodLocatorAttribute.Parameter.HASHSET:
+                                        toBytesHashSet = method;
+                                        break;
+                                    case MethodLocatorAttribute.Parameter.DICTIONARY:
 										toBytesDictionary = method;
 										break;
 									default:
@@ -1723,7 +1733,7 @@ namespace CLARTE.Serialization
 
             CheckDeserializationParameters(buffer, start);
 
-            // Read number of elements in array
+            // Read number of elements in list
             read = FromBytes(buffer, start, out list_size);
 
             if(read != uintSize)
@@ -1735,7 +1745,7 @@ namespace CLARTE.Serialization
             {
                 T value;
 
-                // Create the final destination array
+                // Create the final destination list (with correct capacity)
                 list = new List<T>((int) list_size);
 
                 // Get the correct type overload to use
@@ -1746,7 +1756,7 @@ namespace CLARTE.Serialization
                 {
                     read += FromBytesWrapper(buffer, start + read, out value, type);
 
-                    // Save the correctly type value in the output array
+                    // Save the correctly type value in the output list
                     list.Add(value);
                 }
             }
@@ -1769,7 +1779,7 @@ namespace CLARTE.Serialization
 
             CheckSerializationParameters(buffer, start);
 
-            // If array is not defined, just write the length = 0 to the stream
+            // If list is not defined, just write the length = 0 to the stream
             if(list == null)
             {
                 written = ToBytes(ref buffer, start, 0u);
@@ -1806,6 +1816,114 @@ namespace CLARTE.Serialization
                 for(uint i = 0; i < list_size; ++i)
                 {
                     written += ToBytesWrapper(ref buffer, start + written, list[(int) i], type);
+                }
+            }
+
+            return written;
+        }
+        #endregion
+
+        #region HashSets
+        /// <summary>
+        /// Deserialize an hashset of supported objects.
+        /// </summary>
+        /// <typeparam name="T">The type of objects in the hashset.</typeparam>
+        /// <param name="buffer">The buffer containing the serialized data.</param>
+        /// <param name="start">The start index in the buffer of the serialized object.</param>
+        /// <param name="hashset">The deserialized hashset.</param>
+        /// <returns>The number of deserialized bytes.</returns>
+        [MethodLocator(MethodLocatorAttribute.Type.FROM_BYTES, MethodLocatorAttribute.Parameter.HASHSET)]
+        public uint FromBytes<T>(Buffer buffer, uint start, out HashSet<T> hashset)
+        {
+            uint hashset_size, read;
+
+            hashset = null;
+
+            CheckDeserializationParameters(buffer, start);
+
+            // Read number of elements in hashset
+            read = FromBytes(buffer, start, out hashset_size);
+
+            if(read != uintSize)
+            {
+                throw new FormatException(string.Format("The number of read bytes does not match the expected count. Read {0} bytes instead of {1}.", read, uintSize));
+            }
+
+            if(hashset_size > 0)
+            {
+                T value;
+
+                // Create the final destination hashset
+                hashset = new HashSet<T>();
+
+                // Get the correct type overload to use
+                SupportedTypes type = GetSupportedType(typeof(T));
+
+                // Read each element one after another
+                for(uint i = 0; i < hashset_size; ++i)
+                {
+                    read += FromBytesWrapper(buffer, start + read, out value, type);
+
+                    // Save the correctly type value in the output hashset
+                    hashset.Add(value);
+                }
+            }
+
+            return read;
+        }
+
+        /// <summary>
+        /// Serialize an hashset of supported objects.
+        /// </summary>
+        /// <typeparam name="T">The type of objects in the list.</typeparam>
+        /// <param name="buffer">The buffer where to serialize the data.</param>
+        /// <param name="start">The start index in the buffer where to serialize the data.</param>
+        /// <param name="list">The hashset to serialize.</param>
+        /// <returns>The number of serialized bytes.</returns>
+        [MethodLocator(MethodLocatorAttribute.Type.TO_BYTES, MethodLocatorAttribute.Parameter.HASHSET)]
+        public uint ToBytes<T>(ref Buffer buffer, uint start, HashSet<T> hashset)
+        {
+            uint written;
+
+            CheckSerializationParameters(buffer, start);
+
+            // If hashset is not defined, just write the length = 0 to the stream
+            if(hashset == null)
+            {
+                written = ToBytes(ref buffer, start, 0u);
+
+                if(written != uintSize)
+                {
+                    throw new FormatException(string.Format("The number of written bytes does not match the expected count. Wrote {0} bytes instead of {1}.", written, uintSize));
+                }
+            }
+            else
+            {
+                uint type_size;
+
+                uint hashset_size = (uint) hashset.Count;
+
+                // Get the correct type overload to use
+                SupportedTypes type = GetSupportedType(typeof(T));
+
+                if(sizes.TryGetValue(type, out type_size)) // If the type size is not defined, we will need to use on-the-fly buffer resizing, which is less effective.
+                {
+                    // Check wether our buffer is large enough to get all data
+                    ResizeBuffer(ref buffer, start + uintSize + hashset_size * type_size);
+                }
+
+                // Write the length of the hashset in the buffer
+                written = ToBytes(ref buffer, start, hashset_size);
+
+                if(written != uintSize)
+                {
+                    throw new FormatException(string.Format("The number of written bytes does not match the expected count. Wrote {0} bytes instead of {1}.", written, uintSize));
+                }
+
+                // Write all data in the buffer
+                foreach(T value in hashset)
+                {
+                    written += ToBytesWrapper(ref buffer, start + written, value, type);
                 }
             }
 
@@ -2092,13 +2210,21 @@ namespace CLARTE.Serialization
             {
                 result = SupportedTypes.ARRAY;
             }
-#if UNITY_WSA && !UNITY_EDITOR
+#if NETFX_CORE
 			else if(type.GetTypeInfo().IsGenericType && type.GetTypeInfo().GetGenericTypeDefinition() == typeof(List<>))
 #else
             else if(type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>))
 #endif
             {
                 result = SupportedTypes.LIST;
+            }
+#if NETFX_CORE
+			else if(type.GetTypeInfo().IsGenericType && type.GetTypeInfo().GetGenericTypeDefinition() == typeof(HashSet<>))
+#else
+            else if(type.IsGenericType && type.GetGenericTypeDefinition() == typeof(HashSet<>))
+#endif
+            {
+                result = SupportedTypes.HASHSET;
             }
 #if NETFX_CORE
 			else if(type.GetTypeInfo().IsGenericType && type.GetTypeInfo().GetGenericTypeDefinition() == typeof(Dictionary<,>))
@@ -2169,6 +2295,17 @@ namespace CLARTE.Serialization
                     parameters[2] = null;
 
                     read = (uint) fromBytesList.MakeGenericMethod(typeof(T).GetGenericArguments()).Invoke(this, parameters);
+
+                    value = (T) parameters[2];
+                    break;
+                case SupportedTypes.HASHSET:
+                    parameters = new object[nbParameters];
+
+                    parameters[0] = buffer;
+                    parameters[1] = start;
+                    parameters[2] = null;
+
+                    read = (uint) fromBytesHashSet.MakeGenericMethod(typeof(T).GetGenericArguments()).Invoke(this, parameters);
 
                     value = (T) parameters[2];
                     break;
@@ -2351,6 +2488,18 @@ namespace CLARTE.Serialization
                     parameters[2] = value;
 
                     written = (uint) toBytesList.MakeGenericMethod(typeof(T).GetGenericArguments()).Invoke(this, parameters);
+
+                    buffer = (Buffer) parameters[0];
+
+                    break;
+                case SupportedTypes.HASHSET:
+                    parameters = new object[nbParameters];
+
+                    parameters[0] = buffer;
+                    parameters[1] = start;
+                    parameters[2] = value;
+
+                    written = (uint) toBytesHashSet.MakeGenericMethod(typeof(T).GetGenericArguments()).Invoke(this, parameters);
 
                     buffer = (Buffer) parameters[0];
 
