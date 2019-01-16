efd2fe7be241f2a353638da6514e14b2d7c6708a
Teddy Payen <teddy@clarte.asso.fr>
2019-01-16 17:53:04 +0100

    added Collision with returned direction and distance (compute needed) and Collision2D without  returned direction and distance

--- Geometry/Collision/BoxSAT.cs
+++ Geometry/Collision/BoxSAT.cs
@@ -199,6 +199,237 @@ namespace CLARTE.Geometry.Collision
         }
 
         /// <summary>
+        /// Detected collision between two OOBB using the Separating Axis Theorem (SAT) algorithm.
+        /// </summary>
+        /// <param name="a">The first box.</param>
+        /// <param name="b">The second box.</param>
+        /// <returns>True if the two boxes are colliding, false otherwise.</returns>
+        /// TODO: evaluate direction and distance
+        public static bool Collision(this BoxCollider a, BoxCollider b, out Vector3 direction, out float distance)
+        {
+            direction = Vector3.zero;
+            distance = 0;
+
+            Transform a_transform = a.transform;
+            Transform b_transform = b.transform;
+
+            Vector3 a_axis_x = a_transform.right;
+            Vector3 a_axis_y = a_transform.up;
+            Vector3 a_axis_z = a_transform.forward;
+            Vector3 b_axis_x = b_transform.right;
+            Vector3 b_axis_y = b_transform.up;
+            Vector3 b_axis_z = b_transform.forward;
+
+            // Get all the 15 test axis to use for Box-Box collision test using SAT
+            Vector3[] axes = new Vector3[]
+            {
+                a_axis_x,
+                a_axis_y,
+                a_axis_z,
+                b_axis_x,
+                b_axis_y,
+                b_axis_z,
+                Vector3.Cross(a_axis_x, b_axis_x),
+                Vector3.Cross(a_axis_x, b_axis_y),
+                Vector3.Cross(a_axis_x, b_axis_z),
+                Vector3.Cross(a_axis_y, b_axis_x),
+                Vector3.Cross(a_axis_y, b_axis_y),
+                Vector3.Cross(a_axis_y, b_axis_z),
+                Vector3.Cross(a_axis_z, b_axis_x),
+                Vector3.Cross(a_axis_z, b_axis_y),
+                Vector3.Cross(a_axis_z, b_axis_z)
+            };
+
+            // Get the corners of each box
+            Vector3[] a_corners = a.GetCorners(Space.World);
+            Vector3[] b_corners = b.GetCorners(Space.World);
+
+            int nb_axes = axes.Length;
+            int nb_corners = a_corners.Length;
+
+            // Test overlap on each axis
+            for (int i = 0; i < nb_axes; i++)
+            {
+                Vector3 axis = axes[i];
+
+                // Cross product = (0, 0, 0) => collinear base vectors
+                // i.e. box alligned on some axis: we can safely skip the test on this degenerated axis
+                if (axis != Vector3.zero)
+                {
+                    float a_proj_min = float.MaxValue;
+                    float a_proj_max = float.MinValue;
+                    float b_proj_min = float.MaxValue;
+                    float b_proj_max = float.MinValue;
+
+                    // Get min and max value of projected corners into current axis
+                    for (int j = 0; j < nb_corners; j++)
+                    {
+                        float a_proj = Vector3.Dot(a_corners[j], axis);
+                        float b_proj = Vector3.Dot(b_corners[j], axis);
+
+                        if (a_proj < a_proj_min)
+                        {
+                            a_proj_min = a_proj;
+                        }
+
+                        if (a_proj > a_proj_max)
+                        {
+                            a_proj_max = a_proj;
+                        }
+
+                        if (b_proj < b_proj_min)
+                        {
+                            b_proj_min = b_proj;
+                        }
+
+                        if (b_proj > b_proj_max)
+                        {
+                            b_proj_max = b_proj;
+                        }
+                    }
+
+                    bool overlap = false;
+
+                    // Test if projections are overlaping on the current axis
+                    if (a_proj_min == a_proj_max)
+                    {
+                        overlap = true;
+                    }
+                    else if (a_proj_min < b_proj_min)
+                    {
+                        if (a_proj_max >= b_proj_min)
+                        {
+                            overlap = true;
+                        }
+                    }
+                    else
+                    {
+                        if (b_proj_max >= a_proj_min)
+                        {
+                            overlap = true;
+                        }
+                    }
+
+                    // No collision if boxes does not overlap on at least one axis
+                    if (!overlap)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // All tested axis does overlap, therefore the boxes collide
+            return true;
+        }
+
+        /// <summary>
+		/// Detected collision between two OOBB using the Separating Axis Theorem (SAT) algorithm.
+		/// </summary>
+		/// <param name="a">The first box.</param>
+		/// <param name="b">The second box.</param>
+		/// <returns>True if the two boxes are colliding, false otherwise.</returns>
+		public static bool Collision2D(this BoxCollider a, BoxCollider b)
+        {
+            Transform a_transform = a.transform;
+            Transform b_transform = b.transform;
+
+            Vector3 a_axis_x = (a_transform.right);
+            Vector3 a_axis_y = (a_transform.up);
+            Vector3 b_axis_x = (b_transform.right);
+            Vector3 b_axis_y = (b_transform.up);
+
+            // Get all the 4 test axis to use for Box-Box collision test using SAT
+            Vector3[] axes = new Vector3[]
+            {
+                a_axis_x,
+                a_axis_y,
+                b_axis_x,
+                b_axis_y,
+            };
+
+            // Get the corners of each box
+            Vector3[] a_corners = a.GetCorners2D(Space.World);
+            Vector3[] b_corners = b.GetCorners2D(Space.World);
+
+            int nb_axes = axes.Length;
+            int nb_corners = a_corners.Length;
+
+            // Test overlap on each axis
+            for (int i = 0; i < nb_axes; i++)
+            {
+                Vector3 axis = (axes[i]);
+
+                // Cross product = (0, 0, 0) => collinear base vectors
+                // i.e. box alligned on some axis: we can safely skip the test on this degenerated axis
+                if (axis != Vector3.zero)
+                {
+                    float a_proj_min = float.MaxValue;
+                    float a_proj_max = float.MinValue;
+                    float b_proj_min = float.MaxValue;
+                    float b_proj_max = float.MinValue;
+
+                    // Get min and max value of projected corners into current axis
+                    for (int j = 0; j < nb_corners; j++)
+                    {
+                        float a_proj = Vector3.Dot(Camera.main.WorldToScreenPoint(a_corners[j]), axis);
+                        float b_proj = Vector3.Dot(Camera.main.WorldToScreenPoint(b_corners[j]), axis);
+
+                        if (a_proj < a_proj_min)
+                        {
+                            a_proj_min = a_proj;
+                        }
+
+                        if (a_proj > a_proj_max)
+                        {
+                            a_proj_max = a_proj;
+                        }
+
+                        if (b_proj < b_proj_min)
+                        {
+                            b_proj_min = b_proj;
+                        }
+
+                        if (b_proj > b_proj_max)
+                        {
+                            b_proj_max = b_proj;
+                        }
+                    }
+
+                    bool overlap = false;
+
+                    // Test if projections are overlaping on the current axis
+                    if (a_proj_min == a_proj_max)
+                    {
+                        overlap = true;
+                    }
+                    else if (a_proj_min < b_proj_min)
+                    {
+                        if (a_proj_max >= b_proj_min)
+                        {
+                            overlap = true;
+                        }
+                    }
+                    else
+                    {
+                        if (b_proj_max >= a_proj_min)
+                        {
+                            overlap = true;
+                        }
+                    }
+
+                    // No collision if boxes does not overlap on at least one axis
+                    if (!overlap)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // All tested axis does overlap, therefore the boxes collide
+            return true;
+        }
+
+        /// <summary>
 		/// Detected collision between two OOBB using the Separating Axis Theorem (SAT) algorithm.
 		/// </summary>
 		/// <param name="a">The first box.</param>
@@ -310,7 +541,7 @@ namespace CLARTE.Geometry.Collision
             float a_y_min = Camera.main.WorldToViewportPoint(a_corners[0]).y;
             float a_x_max = Camera.main.WorldToViewportPoint(a_corners[3]).x;
             float a_y_max = Camera.main.WorldToViewportPoint(a_corners[3]).y;
-                                        
+
             float b_x_min = Camera.main.WorldToViewportPoint(b_corners[0]).x;
             float b_y_min = Camera.main.WorldToViewportPoint(b_corners[0]).y;
             float b_x_max = Camera.main.WorldToViewportPoint(b_corners[3]).x;
@@ -349,16 +580,16 @@ namespace CLARTE.Geometry.Collision
             //    distY = b_y_max - a_y_min;
             //}
 
-            if (distX < distY && distX > 0)
-            {
-                distance = Mathf.Abs(distX);
-                direction = dirX;
-            }
-            else if (distY < distX && distY > 0)
-            {
-                distance = Mathf.Abs(distY);
-                direction = dirY;
-            }
+            //if (distX < distY && distX > 0)
+            //{
+            //    distance = Mathf.Abs(distX);
+            //    direction = dirX;
+            //}
+            //else if (distY < distX && distY > 0)
+            //{
+            //    distance = Mathf.Abs(distY);
+            //    direction = dirY;
+            //}
 
             direction = dirX * distX + dirY * distY;
 
