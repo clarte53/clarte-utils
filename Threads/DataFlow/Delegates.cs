cdb145fb069a6890615bd0cb62bcecc5fe887929
Leo Terziman <leo.terziman@clarte.asso.fr>
2019-09-05 10:08:57 +0200

    Started API documentation

--- Threads/DataFlow/Delegates.cs
+++ Threads/DataFlow/Delegates.cs
@@ -1,16 +1,59 @@
 ﻿namespace CLARTE.Threads.DataFlow {
-    public delegate OutputType CreateDataDelegate<OutputType>();
-    public delegate OutputType WorkOnDataDelegate<InputType, OutputType>(InputType data);
+	#region Delegates
+	/// <summary>
+	/// Delegate for functions providing data to the flow.
+	/// </summary>
+	/// <typeparam name="OutputType">The type on data provided.</typeparam>
+	/// <returns>Some data for the flow.</returns>
+	public delegate OutputType CreateDataDelegate<OutputType>();
+
+	/// <summary>
+	/// Delegate for functions that transform data in the flow.
+	/// </summary>
+	/// <typeparam name="InputType">The type of data before transformation.</typeparam>
+	/// <typeparam name="OutputType">The type of data after transformation.</typeparam>
+	/// <param name="data">The data to transform.</param>
+	/// <returns>The transformed data.</returns>
+	public delegate OutputType WorkOnDataDelegate<InputType, OutputType>(InputType data);
+
+	/// <summary>
+	/// Delegate for functions that receive transformed data from the flow.
+	/// </summary>
+	/// <typeparam name="InputType">The type of transformed data.</typeparam>
+	/// <param name="data">The transformed data.</param>
     public delegate void ConsumerDataDelegate<InputType>(InputType data);
 
-    // Data transmission between chained workers
-    public delegate void ProvideDataDelegate<OutputType>(OutputType data, bool clone);
+	/// <summary>
+	/// Delegate function to clone data when multiple consumers are linked to the same output of a provider. 
+	/// </summary>
+	/// <remarks>Data transmission between chained workers.</remarks>
+	/// <typeparam name="OutputType">The type of the data.</typeparam>
+	/// <param name="data">The data returned by the provider.</param>
+	/// <param name="clone">True if the data should be cloned, false otherwise.</param>
+	public delegate void ProvideDataDelegate<OutputType>(OutputType data, bool clone);
+	#endregion
 
-    public interface IDataProvider<OutputType> {
+	#region Interfaces
+	/// <summary>
+	/// Interface for data providers.
+	/// </summary>
+	/// <typeparam name="OutputType">The type of provided data.</typeparam>
+	public interface IDataProvider<OutputType> {
+		/// <summary>
+		/// The list of registered output receivers for this provider.
+		/// </summary>
         event ProvideDataDelegate<OutputType> ProvideDataEvent;
     }
 
-    public interface IMonoBehaviourDataProvider<OuptputType> {
+	/// <summary>
+	/// Interface for data providers based on Unity MonoBehaviours.
+	/// </summary>
+	/// <typeparam name="OuptputType">The type of provided data.</typeparam>
+	public interface IMonoBehaviourDataProvider<OuptputType> {
+		/// <summary>
+		/// Getter for associated IDataProvider.
+		/// </summary>
         IDataProvider<OuptputType> Provider { get; }
     }
+	#endregion
 }
