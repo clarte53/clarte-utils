dbfa97bff113c6cd6b614d0a9d30736178683870
Leo Terziman <leo.terziman@clarte.asso.fr>
2019-09-05 11:40:02 +0200

    End of DataFlow API doc

--- Threads/DataFlow/Unity/DataConsumer.cs
+++ Threads/DataFlow/Unity/DataConsumer.cs
@@ -1,48 +1,90 @@
 ﻿using System;
 using UnityEngine;
 
-namespace CLARTE.Threads.DataFlow.Unity {
-    public abstract class DataConsumer<InputType>: MonoBehaviour where InputType : ICloneable {
-        [SerializeField] protected GameObject DataProvider;
-        [SerializeField] protected DataBarrier DataBarrier;
-        [SerializeField] protected int millisecondsTimeout = 1000;
+namespace CLARTE.Threads.DataFlow.Unity
+{
+    public abstract class DataConsumer<InputType>: MonoBehaviour where InputType : ICloneable
+	{
+		#region Members
+		/// <summary>
+		/// IMonoBehaviourDataProvider that provides input data for this object.
+		/// </summary>
+		[SerializeField] protected GameObject DataProvider;
 
-        protected DataFlow.DataConsumer<InputType> consumer;
+		/// <summary>
+		/// Optional barrier to wait at.
+		/// </summary>
+		[SerializeField] protected DataBarrier DataBarrier;
 
-        protected virtual void OnValidate() {
-            if (DataProvider != null) {
+		/// <summary>
+		/// Timeout used to raise exception if no data have been received in a given amount of time.
+		/// </summary>
+		[SerializeField] protected int millisecondsTimeout = 1000;
+
+		/// <summary>
+		/// Encapsulated DataConsumer.
+		/// </summary>
+		protected DataFlow.DataConsumer<InputType> consumer;
+		#endregion
+
+		#region Abstract methods
+		/// <summary>
+		/// ConsumerDataDelegate implementation to receive data.
+		/// </summary>
+		/// <param name="data">The received data.</param>
+		protected abstract void ConsumeData(InputType data);
+		#endregion
+
+		#region MonoBehaviour callbacks
+		protected virtual void OnValidate()
+		{
+            if (DataProvider != null)
+			{
                 IMonoBehaviourDataProvider<InputType> monoprovider = DataProvider.GetComponent<IMonoBehaviourDataProvider<InputType>>();
-                if (monoprovider == null) {
+
+                if (monoprovider == null)
+				{
                     DataProvider = null;
                 }
             }
         }
 
-        protected void Awake() {
-            // Create consumer
-            consumer = new DataFlow.DataConsumer<InputType>();
-            consumer.millisecondsTimeout = millisecondsTimeout;
-            consumer.ConsumeData = ConsumeData;
-            if (DataBarrier != null) {
+        protected void Awake()
+		{
+			// Create consumer
+			consumer = new DataFlow.DataConsumer<InputType>
+			{
+				millisecondsTimeout = millisecondsTimeout,
+				ConsumeData = ConsumeData
+			};
+
+			if (DataBarrier != null)
+			{
                 consumer.RegisterBarrier(DataBarrier.barrier);
             }
         }
 
-        protected virtual void Start() {
-            if (DataProvider != null) {
-                var monoProvider = DataProvider.GetComponent<IMonoBehaviourDataProvider<InputType>>();
+        protected virtual void Start()
+		{
+            if (DataProvider != null)
+			{
+				IMonoBehaviourDataProvider<InputType> monoProvider = DataProvider.GetComponent<IMonoBehaviourDataProvider<InputType>>();
+
                 monoProvider.Provider.ProvideDataEvent += consumer.EnqeueTask;
-            } else {
+            }
+			else
+			{
                 throw new NoDataProviderException("You have to set a DataProvider in Unity or in Awake.");
             }
         }
 
-        protected virtual void Update() {
-            if (consumer.HasException) {
+        protected virtual void Update()
+		{
+            if (consumer.HasException)
+			{
                 consumer.Throw();
             }
         }
-
-        protected abstract void ConsumeData(InputType data);
-    }
+		#endregion
+	}
 }
