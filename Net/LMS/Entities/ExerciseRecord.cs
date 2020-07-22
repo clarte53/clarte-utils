c30c8f80eecb36e2c3e54c388a13668af6e14533
Leo Terziman <leo.terziman@clarte.asso.fr>
2020-07-22 16:28:23 +0200

    Fixed LMS API dates to use UTC

--- Net/LMS/Entities/ExerciseRecord.cs
+++ Net/LMS/Entities/ExerciseRecord.cs
@@ -8,7 +8,7 @@ namespace CLARTE.Net.LMS.Entities
         public long id;
         public long user;
         public long exercise;
-        public long date; // In ticks, i.e. the number of 100-nanosecond intervals that have elapsed since January 1, 0001 at 00:00:00.000 in the Gregorian calendar.
+        public long date; // In ticks, i.e. the number of 100-nanosecond intervals that have elapsed since January 1, 0001 at 00:00:00.000 in the Gregorian calendar UTC.
         public uint duration; // In seconds
         public float grade;
         public bool success;
--- Net/LMS/Entities/ModuleSummary.cs
+++ Net/LMS/Entities/ModuleSummary.cs
@@ -8,8 +8,8 @@ namespace CLARTE.Net.LMS.Entities
 	{
 		public long user;
 		public long module;
-		public long firstUseDate; // In ticks, i.e. the number of 100-nanosecond intervals that have elapsed since January 1, 0001 at 00:00:00.000 in the Gregorian calendar.
-		public long completionDate; // In ticks, i.e. the number of 100-nanosecond intervals that have elapsed since January 1, 0001 at 00:00:00.000 in the Gregorian calendar.
+		public long firstUseDate; // In ticks, i.e. the number of 100-nanosecond intervals that have elapsed since January 1, 0001 at 00:00:00.000 in the Gregorian calendar UTC.
+		public long completionDate; // In ticks, i.e. the number of 100-nanosecond intervals that have elapsed since January 1, 0001 at 00:00:00.000 in the Gregorian calendar UTC.
 		public uint duration; // In seconds
 		public uint nbChallengesValidated;
 		public List<ExerciseSummary> exercises;
